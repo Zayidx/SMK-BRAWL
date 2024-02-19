2c3819866ba84352d0aed3b07dc3bbb758f10dee
Akmxyyxd683 <152455534+Akmxyyxd683@users.noreply.github.com>
2024-02-19 19:41:15 +0700

    Add files via upload

---
+++ Player.gd
@@ -0,0 +1,64 @@
+class_name PlayerEntity
+extends CharacterBody2D
+
+@export var speed = 400
+@export var gravity = 30
+@export var jump_force = 800
+@onready var anim = get_node("AnimatedSprite")
+
+func _physics_process(_delta):
+	print($Player/CollisionShape2D2.disabled)
+	velocity.x = 0
+	if !is_on_floor():
+		velocity.y += gravity
+		if velocity.y > 1500:
+			velocity.y = 500
+			
+	if Input.is_action_just_pressed("jump") && is_on_floor() && !is_attacking():
+		anim.play("jump")
+		velocity.y = -jump_force
+	elif Input.is_action_pressed("move_left") && !is_attacking():
+		
+		anim.play("Run")
+		anim.flip_h = true
+		velocity.x = -speed
+	elif Input.is_action_pressed("move_right") && !is_attacking():
+
+		anim.play("Run")
+		velocity.x = speed
+		anim.flip_h = false
+	
+	elif is_on_floor() :
+		if(!is_attacking()):
+			anim.play("idle")
+	if Input.is_action_just_pressed("attack"):
+		anim.play("attack")
+		$Player/CollisionShape2D2.disabled = false
+	if !is_attacking():
+		$Player/CollisionShape2D2.disabled = true
+		
+		
+
+	
+	
+	
+	move_and_slide()
+
+
+
+func _on_area_2d_body_entered(body):
+	print("Terkena object")
+	if body.is_in_group("Enemy"):
+		body.queue_free()
+
+
+func is_attacking():
+	if anim.animation == "attack" && anim.frame < 1:
+		return true
+	else: 
+		return false
+
+
+func _on_Player_body_entered(body):
+	if body.is_in_group("enemy"):
+		body._take_damage(20)
