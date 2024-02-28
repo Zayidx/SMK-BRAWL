fd62343d67d86329a0c3fa9e3bde7a41d934cfe9
Akmxyyxd683 <152455534+Akmxyyxd683@users.noreply.github.com>
2024-02-28 11:38:41 +0700

    Add files via upload

--- Player.gd
+++ Player.gd
@@ -1,11 +1,18 @@
 class_name PlayerEntity
 extends CharacterBody2D
+@export var knockbackStrength = 800
+var dir = 1
+signal knockback
 
 @export var speed = 400
 @export var gravity = 30
-@export var jump_force = 800
+@export var jump_force = 600
 @onready var anim = get_node("AnimatedSprite")
-var hp = 100
+@export var hp = 2500
+var motion = Vector2()
+var knockback_dir= Vector2()
+var knockback_wait = 10
+const UP =Vector2(0,-1)
 
 func _physics_process(_delta):
 	print($Player/CollisionShape2D2.disabled)
@@ -18,18 +25,17 @@ func _physics_process(_delta):
 			velocity.y = 500
 			
 	if Input.is_action_just_pressed("jump") && is_on_floor() && !is_attacking():
-		anim.play("jump")
 		velocity.y = -jump_force
-	elif Input.is_action_pressed("move_left") && !is_attacking():
-		
+	elif Input.is_action_pressed("move_left") && !is_attacking():	
 		anim.play("Run")
 		anim.flip_h = true
 		velocity.x = -speed
+		dir = -1
 	elif Input.is_action_pressed("move_right") && !is_attacking():
-
 		anim.play("Run")
 		velocity.x = speed
 		anim.flip_h = false
+		dir = 1
 	
 	elif is_on_floor() :
 		if(!is_attacking()):
@@ -40,14 +46,23 @@ func _physics_process(_delta):
 	if !is_attacking():
 		$Player/CollisionShape2D2.disabled = true
 		
-		
-
+	if velocity.y <0:
+		anim.play("jump")
+	if velocity.y >0:
+		anim.play("turun")
+	
+	for body in $Player.get_overlapping_bodies():
+		if knockback_wait <= 0 and body.get("NAME") == "enemy":
+			emit_signal("knockback") 
+			knockback_wait = 10
+	knockback_wait -= 1
 	
 	
-	
+	update_health()
 	move_and_slide()
 
-
+func attack_with_knockback():
+	pass
 
 func _on_area_2d_body_entered(body):
 	print("Terkena object")
@@ -56,7 +71,7 @@ func _on_area_2d_body_entered(body):
 
 
 func is_attacking():
-	if anim.animation == "attack" && anim.frame < 1:
+	if anim.animation == "attack" && anim.frame < 4:
 		return true
 	else: 
 		return false
@@ -65,6 +80,42 @@ func is_attacking():
 func _on_Player_body_entered(body):
 	if body.is_in_group("enemy"):
 		body._take_damage(20)
+		if anim.flip_h == false :
+			body.velocity.x = knockbackStrength
+		else:
+			body.velocity.x = -knockbackStrength
 
 func take_damage(damage):
 	hp -= damage
+	if $TimerTakeDamage.is_stopped():
+		$AnimatedSprite.material.set_shader_parameter("opacity", 1.0);
+		$AnimatedSprite.material.set_shader_parameter("r", 1.0);
+		$AnimatedSprite.material.set_shader_parameter("g", 0);
+		$AnimatedSprite.material.set_shader_parameter("b", 0);
+		$AnimatedSprite.material.set_shader_parameter("mix_color", 0.7);
+		$TimerTakeDamage.start(0)
+
+func update_health():
+	var healthbar = $healthbar
+	
+	healthbar.value = hp
+	
+	if hp >= 2500:
+		healthbar.visible = true
+	else:
+		healthbar.visible = true
+
+
+func _on_timer_take_damage_timeout():
+	$TimerTakeDamage.stop()
+	$AnimatedSprite.material.set_shader_parameter("opacity", 1.0);
+	$AnimatedSprite.material.set_shader_parameter("mix_color", 0)
+
+
+func _on_regen_timer_timeout():
+	if hp < 2500:
+		hp = hp + 250
+		if hp > 2500:
+			hp = 2500
+	if hp == 0:
+		hp = 0
--- SMK-BRAWL-akmal/Enemy.gd
+++ SMK-BRAWL-akmal/Enemy.gd
@@ -1,10 +1,7 @@
 extends CharacterBody2D
-const NAME = "enemy"
-@export var enemydamage = 500
-var damage = enemydamage / 2500
 @export var gravity = 30
 @export var hp = 100
-var attack = true
+
 @onready var wall_raycast_left = $Wall_Checks/Wall_Raycast_Left as RayCast2D
 @onready var wall_raycast_right = $Wall_Checks/Wall_Raycast_Right as RayCast2D
 @onready var floor_raycast_left = $Floor_Checks/Floor_Raycast_Left as RayCast2D
@@ -14,19 +11,14 @@ var attack = true
 @onready var Sprite_2d = $Sprite_2d as Sprite2D
 @onready var chase_timer = $Chase_Timer as Timer
 
-@onready var anim = $AnimatedSprite2D
 
 @export var wander_speed : float = 40.0
 @export var chase_speed : float = 80.0
 
+
 var current_speed : float  = 0.0
 var player_found : bool = false 
 var player : PlayerEntity = null
-var is_attack_ready = false
-
-var dir = 1
-var knockback_dir
-var knockback
 
 enum states{
 	Wander,
@@ -36,12 +28,10 @@ enum states{
 var current_state = states.Wander
 
 func _ready():
-	anim.play("walk")
 	player = get_tree().get_first_node_in_group("player")
 	chase_timer.timeout.connect(on_timer_timeout)
 
 func _physics_process(_delta):
-	update_health()
 	handle_vision()
 	track_player()
 	handle_movement()
@@ -54,17 +44,9 @@ func _physics_process(_delta):
 		velocity.y += gravity
 		if velocity.y > 1500:
 			velocity.y = 500
-	if $attack_delay.time_left == $attack_delay.wait_time:
-		is_attack_ready = true
-	else:
-		is_attack_ready = false 
 	
 func handle_movement() -> void:
-	var direction = null
-	if player != null && direction == null:
-		direction = global_position - player.global_position
-	else:
-		return
+	var direction = global_position - player.global_position
 	
 	if current_state == states.Wander:
 		if floor_raycast_right.is_colliding() != true: 
@@ -80,28 +62,15 @@ func handle_movement() -> void:
 		if player_found == true:
 			if direction.x < 0:
 				current_speed = chase_speed
-				anim.flip_h = false
-				if is_attacking() == false:
-					anim.play("walk")
-				
 			else:
 				current_speed = -chase_speed
-				
-				
-				anim.flip_h = true
-				
-				if is_attacking() == false:
-					anim.play("walk")
+
+
 	velocity.x = current_speed
 
 func track_player():
 	if player == null:
 		return
-		
-	if global_position.distance_to(player.global_position) < 78 :
-		anim.play("attack")
-		player.take_damage(10)
-		
 		
 	var direction_to_player : Vector2 = Vector2(player.position.x, player.position.y - 8)\
 	- player_tracker_raycast.position
@@ -109,19 +78,7 @@ func track_player():
 	player_tracker_pivot.look_at(direction_to_player)
 
 func _take_damage(damage):
-	if knockback == true:
-		velocity.y = -200
-		velocity.x = 350 
-		knockback = false
-		velocity.x = -current_speed
 	hp -= damage
-	if $TimerTakeDamage.is_stopped():
-		$AnimatedSprite2D.material.set_shader_parameter("opacity", 1.0);
-		$AnimatedSprite2D.material.set_shader_parameter("r", 1.0);
-		$AnimatedSprite2D.material.set_shader_parameter("g", 0);
-		$AnimatedSprite2D.material.set_shader_parameter("b", 0);
-		$AnimatedSprite2D.material.set_shader_parameter("mix_color", 0.7);
-		$TimerTakeDamage.start(0)
 
 func handle_vision():
 	if player_tracker_raycast.is_colliding():
@@ -135,46 +92,23 @@ func handle_vision():
 			player_found = true
 	else :
 		player_found = false
-		
 
 func on_timer_timeout() -> void:
 	if player_found == false:
 		current_state = states.Wander
 
+
 func _on_enemyarea_body_entered(body):
 	if body.is_in_group("player"):
-		body.take_damage(10)
-		anim.play("attack")
-		
-func _on_enemyarea_body_exited(body):
-	if body.name == "player":
-		attack = false
-		
-func is_attacking():
-	if anim.animation == "attack" && anim.frame > 2:
-		return false
-	else:
-		return true
-		print("true")
-
-func update_health():
-	var healthbar = $healthbar
-	
-	healthbar.value = hp
-	
-	if hp >= 100:
-		healthbar.visible = false
-	else:
-		healthbar.visible = true
-
-func _on_timer_take_damage_timeout():
-	$TimerTakeDamage.stop()
-	$AnimatedSprite2D.material.set_shader_parameter("opacity", 1.0);
-	$AnimatedSprite2D.material.set_shader_parameter("mix_color", 0)
+		body.take_damage(20)
 
 
-func _on_player_knockback():
-	var player_dir = get_parent().get_node("Player").dir 
-	knockback_dir = player_dir
-	dir = knockback_dir * -1
-	knockback = true
+
+
+func _on_attack_delay_timeout():
+	$Timer.start()
+	$Enemyarea/CollisionShape2D.disabled = false
+
+
+func _on_timer_timeout():
+	$Enemyarea/CollisionShape2D.disabled = true
