69b010c1a3a7a3656a97baab67afdd679d414e67
Razputeen <159450181+Razputeen@users.noreply.github.com>
2024-04-30 05:27:26 +0700

    Add files via upload

---
+++ Backsound/audio_player_1.gd
@@ -0,0 +1,14 @@
+extends AudioStreamPlayer
+
+const level_music = preload("res://Backsound/Escort.mp3")
+
+func _play_music(music: AudioStream, volume = -2.0):
+	if stream == music:
+		return
+		
+	stream = music
+	volume_db = volume
+	play()
+
+func play_music_level():
+	_play_music(level_music)
